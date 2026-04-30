6f3cc15371eca9029ec0c2561b542fa7c29caafb
crow1126 <samuelwobil11@gmail.com>
2026-04-30 13:13:22 +0000

    specific player positions

--- app/layout.js
+++ app/layout.js
@@ -24,3 +24,13 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
